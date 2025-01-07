9466bb6237f34072ccb65ab1c784a366eff96913
Rahma <rahma08sep2005@gmail.com>
2025-01-07 11:22:40 +0400

    website

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 4000; 
+
+app.use(cors());
+
+app.use(express.json());
+app.use('/api/user', userRoutes)
+
+const dbURI = process.env.DB_URI;
+
+//const dbURI = "mongodb://localhost:27017"
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURI);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err.message);
+        process.exit(1);
+    }
+
+};
+
+connectDB();
+
+app.listen(port, () =>{
+     console.log(`Server running on port ${port}`);
+});
